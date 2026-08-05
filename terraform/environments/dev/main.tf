ae86d43ec40dd798d9f8113b937aa40c34e38d28
Kapildev Singh <kdsinghse20@gmail.com>
2026-08-06 00:51:14 +0530

    Added terraform vpc Module

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,1 +1,25 @@
-#Terraform
+module "vpc" {
+  source = "../../modules/vpc"
+
+  project_name = var.project_name
+  environment  = var.environment
+
+  vpc_name = "${var.project_name}-${var.environment}"
+
+  vpc_cidr = "10.0.0.0/16"
+
+  public_subnet_cidrs = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+
+  private_app_subnet_cidrs = [
+    "10.0.10.0/24",
+    "10.0.11.0/24"
+  ]
+
+  private_ai_subnet_cidrs = [
+    "10.0.20.0/24",
+    "10.0.21.0/24"
+  ]
+}
---
+++ terraform/modules/vpc/main.tf
@@ -0,0 +1,158 @@
+
+##### VPC ####
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+##### Internat Gatway ####
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.vpc_name}-igw"
+  }
+}
+
+##### Public Subnet ####
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-public-${count.index + 1}"
+
+    "kubernetes.io/role/elb" = "1"
+
+    "kubernetes.io/cluster/${var.project_name}-${var.environment}" = "shared"
+  }
+}
+
+#### Private Subnet ####
+resource "aws_subnet" "private_app" {
+  count = length(var.private_app_subnet_cidrs)
+
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_app_subnet_cidrs[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-private-app-${count.index + 1}"
+
+    "kubernetes.io/role/internal-elb" = "1"
+
+    "kubernetes.io/cluster/${var.project_name}-${var.environment}" = "shared"
+  }
+}
+
+#####Private Subnet AI ####
+resource "aws_subnet" "private_ai" {
+  count = length(var.private_ai_subnet_cidrs)
+
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_ai_subnet_cidrs[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-private-ai-${count.index + 1}"
+
+    "kubernetes.io/role/internal-elb" = "1"
+
+    "kubernetes.io/cluster/${var.project_name}-${var.environment}" = "shared"
+  }
+}
+
+#### Elastic AI ####
+resource "aws_eip" "nat" {
+  count  = length(var.public_subnet_cidrs)
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.vpc_name}-nat-eip-${count.index + 1}"
+  }
+}
+
+
+#### NAT Gatway ####
+resource "aws_nat_gateway" "this" {
+  count         = length(var.public_subnet_cidrs)
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
+
+  depends_on = [
+    aws_internet_gateway.this
+  ]
+
+  tags = {
+    Name = "${var.vpc_name}-nat-${count.index + 1}"
+  }
+}
+
+#### Public Route Table ####
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = {
+    Name = "${var.vpc_name}-public-rt"
+  }
+}
+
+#resource "aws_route_table_association" "public" {
+#  # 1. Loop through your public subnets
+#  count = length(aws_subnet.public)
+#
+#  subnet_id      = aws_subnet.public[count.index].id ####aws_subnet.public.id
+#  route_table_id = aws_route_table.public.id
+#}
+
+#### Private Route Table ####
+resource "aws_route_table" "private" {
+  count  = length(var.public_subnet_cidrs)
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this[count.index].id
+  }
+
+  tags = {
+    Name = "${var.vpc_name}-private-rt-${count.index + 1}"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private_app" {
+  count          = length(var.private_app_subnet_cidrs)
+  subnet_id      = aws_subnet.private_app[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
+
+resource "aws_route_table_association" "private_ai" {
+  count          = length(var.private_ai_subnet_cidrs)
+  subnet_id      = aws_subnet.private_ai[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
+
+
+data "aws_availability_zones" "available" {}
+
+
