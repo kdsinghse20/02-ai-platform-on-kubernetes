8c0e36cb54486687eff5feb38695dcd60671431f
Kapildev Singh <kdsinghse20@gmail.com>
2026-08-07 00:12:30 +0530

    Added module EKS for terraform

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -33,22 +33,6 @@ resource "aws_eks_cluster" "this" {
   }
 }
 
-resource "aws_eks_access_entry" "admin" {
-  cluster_name  = aws_eks_cluster.this.name
-  principal_arn = var.admin_principal_arn
-  type          = "STANDARD"
-}
-
-resource "aws_eks_access_policy_association" "admin" {
-  cluster_name  = aws_eks_cluster.this.name
-  principal_arn = var.admin_principal_arn
-  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-
-  access_scope {
-    type = "cluster"
-  }
-}
-
 resource "aws_eks_node_group" "this" {
 
   cluster_name = aws_eks_cluster.this.name
